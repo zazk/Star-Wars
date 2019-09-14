704ce556bf1a9d3feeed4cfdafdab3369d8c5f55
Enrique Juan de Dios <juandedioz@gmail.com>
2019-09-15 00:23:53 +0200

    [Setup] Setup Grid Component

---
+++ src/components/Grid/Grid.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Grid.css";
+
+const Grid = () => (
+  <div className="grid">
+    <div>Grid with this</div>
+    <div>Grid with this</div>
+    <div>Grid with this</div>
+    <div>Grid with this</div>
+    <div>Grid with this</div>
+    <div>Grid with this</div>
+    <div>Grid with this</div>
+    <div>Grid with this</div>
+    <div>Grid with this</div>
+  </div>
+);
+export default Grid;
