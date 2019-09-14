62836d73cff2bfc291e1c8bdcaaac773c3798d0d
Enrique Juan de Dios <juandedioz@gmail.com>
2019-09-15 01:07:39 +0200

    [Setup] Request API

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Grid.css";
 
-const Grid = () => (
-  <div className="grid">
-    <div>Grid with this</div>
-    <div>Grid with this</div>
-    <div>Grid with this</div>
-    <div>Grid with this</div>
-    <div>Grid with this</div>
-    <div>Grid with this</div>
-    <div>Grid with this</div>
-    <div>Grid with this</div>
-    <div>Grid with this</div>
-  </div>
-);
+const Grid = () => {
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    fetch("https://swapi.co/api/films/")
+      .then(response => response.json())
+      .then(obj => setItems(obj.results));
+  }, []);
+  console.log("RESULTS", items);
+  return (
+    <div className="grid">
+      {items.map((item, index) => (
+        <div key={item.episode_id}>
+          <img
+            width={100}
+            src={`http://w.areminds.com/f/starwars/${index + 1}.jpg`}
+            alt={item.title}
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
 export default Grid;
